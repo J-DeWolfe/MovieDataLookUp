f98ee1e72ee50f96ec99de4aa4341a375b1e2cc9
JADeWolfe <jadewolfe@gmail.com>
2020-03-18 07:09:24 -0400

    Potential Exceptions Preempted

--- src/MovieDataLookup/MovieDataLookup.java
+++ src/MovieDataLookup/MovieDataLookup.java
@@ -1,4 +1,7 @@
 package MovieDataLookup;
+//The OMDb API is a RESTful web service to obtain movie information.
+//All content and images on the site are contributed and maintained by their users.
+//OMDB Documentation: https://readthedocs.org/projects/omdbpy/downloads/pdf/latest/
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -11,9 +14,10 @@ import com.google.gson.reflect.TypeToken;
 
 public class MovieDataLookup
 {	
-	private static Map<String, Object> omdbData;
+	private static Map<String, Object> omdbData = null;
+	static MovieDataLookupGUI gui;
 	
-	private static void getOmdbData(String k, String t) throws IOException
+	private static void getOmdbData(String k, String t, String y) throws IOException
 	{
 		String title = t.replace(' ',  '+'); //Readable to query-able
 		String dataString = "";	
@@ -22,15 +26,16 @@ public class MovieDataLookup
 		{
 			String omdbapi = "http://www.omdbapi.com/";
 			String query = "?apikey=" + k + "&t=" + title;
-			URL url = new URL(omdbapi + query);
+			if (y.length() == 4) query = query + "&y=" + y;
+			
+			URL url = new URL(omdbapi + query); //URL includes query
 			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 			connection.setRequestMethod("GET");
 			connection.connect();
 	        
 			int responseCode = connection.getResponseCode();
-			if (responseCode != 200)
-	        	throw new RuntimeException("HttpResponseOce: " + responseCode);
-	        
+		
+			if (responseCode != 200) throw new RuntimeException("HttpResponse: " + responseCode);
 			else
 	        {
 	        	Scanner scan = new Scanner(url.openStream());
@@ -41,47 +46,54 @@ public class MovieDataLookup
 	    }
 		catch (Exception e) { System.out.println("Exception: " + e); }	
 		
-		/*	Directions for including the  com.google.gson library:
-			https://medium.com/programmers-blockchain/importing-gson-into-eclipse-ec8cf678ad52 */
+		//Directions for including the  com.google.gson library:
+			//https://medium.com/programmers-blockchain/importing-gson-into-eclipse-ec8cf678ad52
 		omdbData = new Gson().fromJson(dataString, new TypeToken<HashMap<String, Object>>(){}.getType());
 	}
 		
-	public static String getMovieDataString(String k, String t) throws IOException
+	public static String getMovieDataString(String k, String t, String y) throws IOException
 	{
-		getOmdbData(k, t);
+		getOmdbData(k, t, y); //Update the data when this is called (e.g by the GUI)
 		
-		if (omdbData.get("Title") == null) return "Movie not found";
-		return "" + ((String)omdbData.get("Title")).toUpperCase() + 
-				"\n" +
-				"\nPlot: " + omdbData.get("Plot") +
-				"\n" +
-				"\nType: " + omdbData.get("Type") +
-				"\nYear: " + omdbData.get("Year") +
-				"\nRated: " + omdbData.get("Rated") +
-				"\nReleased: " + omdbData.get("Released") +
-				"\nBox Office: " + omdbData.get("BoxOffice") +
-				"\nDVD: " + omdbData.get("DVD") +
-				"\nRuntime: " + omdbData.get("Runtime") +
-				"\nGenre: " + omdbData.get("Genre") +
-				"\nDirector: " + omdbData.get("Director") +
-				"\nWriter: " + omdbData.get("Writer") +
-				"\nActors: " + omdbData.get("Actors") +
-				"\nLanguage: " + omdbData.get("Language") +
-				"\nCountry: " + omdbData.get("Country") +
-				"\nProduction: " + omdbData.get("Production") +
-				"\n" +
-				"\nMetascore: " + omdbData.get("Metascore") +
-				"\nIMDB Rating: " + omdbData.get("imdbRating") +
-				"\nAwards: " + omdbData.get("Awards") +
-				"\n" +
-				"\nWebsite: " + omdbData.get("Website");
+		try
+		{
+			if (omdbData.get("Title") == null) return "Movie not found";
+			return "" + ((String)omdbData.get("Title")).toUpperCase() + 
+					"\n" +
+					"\nPlot: " + omdbData.get("Plot") +
+					"\n" +
+					"\nType: " + omdbData.get("Type") +
+					"\nYear: " + omdbData.get("Year") +
+					"\nRated: " + omdbData.get("Rated") +
+					"\nReleased: " + omdbData.get("Released") +
+					"\nBox Office: " + omdbData.get("BoxOffice") +
+					"\nDVD: " + omdbData.get("DVD") +
+					"\nRuntime: " + omdbData.get("Runtime") +
+					"\nGenre: " + omdbData.get("Genre") +
+					"\nDirector: " + omdbData.get("Director") +
+					"\nWriter: " + omdbData.get("Writer") +
+					"\nActors: " + omdbData.get("Actors") +
+					"\nLanguage: " + omdbData.get("Language") +
+					"\nCountry: " + omdbData.get("Country") +
+					"\nProduction: " + omdbData.get("Production") +
+					"\n" +
+					"\nMetascore: " + omdbData.get("Metascore") +
+					"\nIMDB Rating: " + omdbData.get("imdbRating") +
+					"\nAwards: " + omdbData.get("Awards") +
+					"\n" +
+					"\nWebsite: " + omdbData.get("Website");
+		}
+		catch (Exception e) { return "An error has occurred. Make sure your API key is valid."; }
 	}
 	
-	public static String getPosterURL() { return (String)omdbData.get("Poster"); }
+	public static String getPosterURL()
+	{
+		if (omdbData == null) return gui.getDefaultPosterURL();
+		return (String)omdbData.get("Poster");
+	}
 	
 	public static void main(String[] args) throws IOException
 	{
-		MovieDataLookupGUI gui = new MovieDataLookupGUI();
-		
+		gui = new MovieDataLookupGUI();
 	}
 }
--- src/MovieDataLookup/MovieDataLookupGUI.java
+++ src/MovieDataLookup/MovieDataLookupGUI.java
@@ -1,15 +1,16 @@
 package MovieDataLookup;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -17,65 +18,76 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
 
 public class MovieDataLookupGUI extends JFrame implements ActionListener
 {
 	private static final long serialVersionUID = 1L;
-	
+	private String defaultPosterURL = "https://2.bp.blogspot.com/_Yt3M33fzOLA/SGDCeUlyXbI/AAAAAAAAF3I/hnxQJ0rfiNk/s400/ew25poster01.jpg";
 	private JLabel apiKeyPrompt = new JLabel("Enter your OMDB API key:");
 	private JLabel titlePrompt = new JLabel("Enter a title to search for:");
+	private JLabel yearPrompt = new JLabel("Enter the year of release (optional):");
 	private JTextField apiKeyField = new JTextField(8);
+	private JTextField yearField = new JTextField(4);
 	private JTextField titleField = new JTextField(24);
 	private JButton searchButton = new JButton("SEARCH");
-	private JTextArea movieDataOutput = new JTextArea("Enter a title to retrieve movie data.");
-
+	private JTextArea movieDataOutput = new JTextArea("To retrieve data, enter your API key and a movie title .");
 	private BufferedImage moviePoster;
 	private JLabel moviePosterOutput = new JLabel();
+	
+	Color PALE = new Color(249, 233, 172); //Still deciding on a color scheme
+	Color DARK = new Color(61, 84, 108);
+	int width;
+	int margin = 4;
+	int border = 8;
 	
 	MovieDataLookupGUI()
 	{
 		super("Movie Data Lookup");
-		setSize(625, 675);
+		setSize(1000, 1000); //Larger than needed
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
-		Border margins = BorderFactory.createEmptyBorder(8, 8, 8, 8);
-		
-		//TOP
-		JPanel userPanel = new JPanel();
-		userPanel.setBorder(margins);
-		userPanel.setLayout(new GridLayout(3, 1));
 		setVisible(true);
+		width = this.getContentPane().getWidth();
 		
-		JPanel akpIkeySubpanel = new JPanel();
-		akpIkeySubpanel.setLayout(new FlowLayout());
-		akpIkeySubpanel.add(apiKeyPrompt);
-		akpIkeySubpanel.add(apiKeyField);
-		apiKeyField.addActionListener(this);
-		userPanel.add(akpIkeySubpanel);
+		//NORTH SECTION
+		JPanel userPanel = new JPanel();
+		userPanel.setBorder(BorderFactory.createEmptyBorder(border, border, margin, border)); 
+		userPanel.setLayout(new GridLayout(2, 1));
+		userPanel.setBackground(DARK);
 		
-		JPanel titleSubpanel = new JPanel();
-		titleSubpanel.setLayout(new FlowLayout());
-		titleSubpanel.add(titlePrompt);
-		titleSubpanel.add(titleField);
+		JPanel inputSubpanel = new JPanel();
+		inputSubpanel.setLayout(new FlowLayout());
+		
+		//TextFields
+		inputSubpanel.add(apiKeyPrompt);
+		inputSubpanel.add(apiKeyField);
+		inputSubpanel.add(titlePrompt);
+		inputSubpanel.add(titleField);
+		inputSubpanel.add(yearPrompt);
+		inputSubpanel.add(yearField);	
 		titleField.addActionListener(this);
-		userPanel.add(titleSubpanel);
+		apiKeyField.addActionListener(this);
+		yearField.addActionListener(this);
 		
+		inputSubpanel.setBackground(PALE);
+		userPanel.add(inputSubpanel);
+		
+		//Button
 		JPanel buttonSubpanel = new JPanel();
 		buttonSubpanel.setLayout(new FlowLayout());
 		buttonSubpanel.add(searchButton);
 		searchButton.addActionListener(this);
+		buttonSubpanel.setBackground(PALE);
 		userPanel.add(buttonSubpanel);
 		
 		add(userPanel, BorderLayout.NORTH);
 		
-		//MIDDLE
+		//SOUTH SECTION
 		JPanel resultPanel = new JPanel();
-		resultPanel.setBorder(margins);
-		resultPanel.setLayout(new GridLayout(1, 2, 8, 0));
+		resultPanel.setBorder(BorderFactory.createEmptyBorder(margin, border, border, border)); 
+		resultPanel.setLayout(new GridLayout(1, 2, border, 0));
 		
 		resultPanel.add(movieDataOutput);
 		movieDataOutput.setEditable(false);
@@ -84,6 +96,9 @@ public class MovieDataLookupGUI extends JFrame implements ActionListener
 		
 		resultPanel.add(moviePosterOutput);
 		moviePosterOutput.setVerticalAlignment(JLabel.NORTH);
+		showPoster();
+		resultPanel.setBackground(DARK);
+		
 		add(resultPanel, BorderLayout.CENTER);
 	}
 	
@@ -92,25 +107,46 @@ public class MovieDataLookupGUI extends JFrame implements ActionListener
 	{
 		if (e.getSource() == searchButton)
 		{
-			String k = apiKeyField.getText();
-			String t = titleField.getText();
-			
-			try { movieDataOutput.setText(MovieDataLookup.getMovieDataString(k, t)); }
-			catch (IOException e1) {e1.printStackTrace(); }
-			
-			URL posterURL;
-			try
-			{ 
-				posterURL = new URL(MovieDataLookup.getPosterURL());
-				moviePoster = ImageIO.read(posterURL);
-				moviePosterOutput.setIcon(new ImageIcon(moviePoster));
+			if (apiKeyField.getText().trim().length() != 0 && titleField.getText().trim().length() != 0)
+			{
+				String y = "";
+				String k = apiKeyField.getText();
+				String t = titleField.getText();
+				if (yearField.getText().trim().length() != 0)
+					y = yearField.getText();
 				
-			} 
-			catch (MalformedURLException e1) { e1.printStackTrace(); }
-			catch (IOException e1) { e1.printStackTrace(); }
-	
+				try
+				{ 
+					movieDataOutput.setText(MovieDataLookup.getMovieDataString(k, t, y)); 
+				}		
+				catch (IOException e1) {e1.printStackTrace(); }
+				showPoster((String)MovieDataLookup.getPosterURL());
+			}
 		}
 	}
+	
+	public String getDefaultPosterURL() { return defaultPosterURL; }
+	
+	public void showPoster() { showPoster(defaultPosterURL); }
+	
+	public void showPoster(String p)
+	{
+		URL posterURL;
+		
+		try
+		{ 
+			posterURL = new URL(p);
+			moviePoster = ImageIO.read(posterURL);
+			int w = (width/2)-border-margin;
+			float scaleRatio = (float)w/(float)moviePoster.getWidth();
+			int h = (int)(scaleRatio * moviePoster.getHeight());
+			
+			Image scaledMoviePosted = moviePoster.getScaledInstance(w, h, Image.SCALE_SMOOTH);
+			moviePosterOutput.setIcon(new ImageIcon(scaledMoviePosted));
+		} 
+		catch (MalformedURLException e1) { if (p != defaultPosterURL) showPoster(); }
+		catch (IOException e1) { e1.printStackTrace(); }
+	}	
 }
-
+//c87439fd
 
